8773440bbfd9623da34be5ff392383cbed11e8a5
huynhtrinh2304 <trinhpro2304@gmail.com>
2022-06-20 00:41:19 +0700

    Xây dựng UI phần Header #1

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const images = {
+  logo: require('./logo.svg').default,
+  search_icon: require('./search_icon.svg').default,
+  search_icon_bold: require('./search_icon_bold.svg').default,
+};
+
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,57 @@
+import { useState, useRef } from 'react';
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+// import icon images from folder assets
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
+  const iconSearchRef = useRef(null);
+  const [value, setValue] = useState('');
+
+  const handleChangeInput = (e) => {
+    changeIconSearch(e.target.value);
+    setValue((prev) => (prev = e.target.value));
+  };
+
+  const changeIconSearch = (value) => {
+    if (value.length > 0) {
+      iconSearchRef.current.src = images.search_icon_bold;
+    } else {
+      iconSearchRef.current.src = images.search_icon;
+    }
+  };
+
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="TikTok" />
+        </div>
+
+        <div className={cx('block-search')}>
+          <input
+            value={value}
+            onChange={handleChangeInput}
+            placeholder="Search accounts and videos"
+            spellCheck={false}
+          ></input>
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <span className={cx('splitter')}></span>
+          <button className={cx('search-btn')}>
+            <img ref={iconSearchRef} src={images.search_icon} alt="Search" />
+          </button>
+        </div>
+
+        <div className={cx('actions')}></div>
+      </div>
     </header>
   );
 }
