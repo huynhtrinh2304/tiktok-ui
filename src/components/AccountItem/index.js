5e8aed39c2678580ca274f013f65e6600ac1893c
huynhtrinh2304 <trinhpro2304@gmail.com>
2022-06-21 22:59:56 +0700

    Xây dựng UI phần Header #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,28 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://www.shareicon.net/data/128x128/2016/05/29/772559_user_512x512.png"
+        alt="Demo"
+      />
+      <div>
+        <p className={cx('name')}>
+          <span>Asmobillet</span>
+          <FontAwesomeIcon className={cx('icon-check')} icon={faCircleCheck} />
+        </p>
+        <span className={cx('username')}>Nguyễn Hữu Sang</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,24 +1,44 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import classNames from 'classnames/bind';
+//Tippy
+import Tippy from '@tippyjs/react/headless';
+//Fontawesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
 // import icon images from folder assets
 import images from '~/assets/images';
 
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+
 const cx = classNames.bind(styles);
 
 function Header() {
   const iconSearchRef = useRef(null);
-  const [value, setValue] = useState('');
+  const [valueInput, setValueInput] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+
+  // Set show and hide Tippy
+  const [visible, setVisible] = useState(true);
+  const show = () => setVisible(true);
+  const hide = () => setVisible(false);
+
+  // Use check valueInput.lenght > 0 and bold icon search
+  useEffect(() => {
+    handleBoldIconSearch(valueInput);
+  }, [valueInput]);
+
+  useEffect(() => {
+    setTimeout(() => setSearchResult([1, 2, 3, 4]), 0);
+  }, []);
 
   const handleChangeInput = (e) => {
-    changeIconSearch(e.target.value);
-    setValue((prev) => (prev = e.target.value));
+    setValueInput(e.target.value);
   };
 
-  const changeIconSearch = (value) => {
+  const handleBoldIconSearch = (value) => {
     if (value.length > 0) {
       iconSearchRef.current.src = images.search_icon_bold;
     } else {
@@ -26,29 +46,51 @@ function Header() {
     }
   };
 
+  const handleClearInput = () => setValueInput('');
+
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        <div className={cx('logo')}>
-          <img src={images.logo} alt="TikTok" />
-        </div>
+        <img src={images.logo} alt="TikTok" />
 
-        <div className={cx('block-search')}>
-          <input
-            value={value}
-            onChange={handleChangeInput}
-            placeholder="Search accounts and videos"
-            spellCheck={false}
-          ></input>
-          <button className={cx('clear')}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </button>
-          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-          <span className={cx('splitter')}></span>
-          <button className={cx('search-btn')}>
-            <img ref={iconSearchRef} src={images.search_icon} alt="Search" />
-          </button>
-        </div>
+        <Tippy
+          interactive="top-start"
+          render={(attrs) => (
+            <div className={cx('search-results')} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx('search-title')}>Accounts</h4>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>
+          )}
+          visible={visible}
+          onClickOutside={hide}
+        >
+          <div className={cx('block-search')}>
+            <input
+              onFocus={() => setVisible(true)}
+              value={valueInput}
+              onChange={handleChangeInput}
+              placeholder="Search accounts and videos"
+              spellCheck={false}
+            ></input>
+
+            <button className={cx('clear')} onClick={handleClearInput}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+
+            <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+            <span className={cx('splitter')}></span>
+
+            <button className={cx('search-btn')}>
+              <img ref={iconSearchRef} src={images.search_icon} alt="Search" />
+            </button>
+          </div>
+        </Tippy>
 
         <div className={cx('actions')}></div>
       </div>
